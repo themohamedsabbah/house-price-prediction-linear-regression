cf9937a3027b9278a6edf86664cf334a0b30c09d
Mohamed Sabbah <mohamed.farouk@cis.asu.edu.eg>
2026-08-16 01:06:22 +0300

    Full Scnario

--- src/plot.py
+++ src/plot.py
@@ -1,6 +1,22 @@
 import numpy as np
 from cost_functions import compute_cost
 import matplotlib.pyplot as plt
+from scipy.stats import norm
+
+def norm_plot(ax, data):
+    scale = (np.max(data) - np.min(data))*0.2
+    x = np.linspace(np.min(data)-scale,np.max(data)+scale,50)
+    _,bins, _ = ax.hist(data, x, color="xkcd:azure")
+    #ax.set_ylabel("Count")
+
+    mu = np.mean(data);
+    std = np.std(data);
+    dist = norm.pdf(bins, loc=mu, scale = std)
+
+    axr = ax.twinx()
+    axr.plot(bins,dist, color = "orangered", lw=2)
+    axr.set_ylim(bottom=0)
+    axr.axis('off')
 
 def plot_cost_i_w(X,y,hist):
     ws = np.array([ p[0] for p in hist["params"]])
